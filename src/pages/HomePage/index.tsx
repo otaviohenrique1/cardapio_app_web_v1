65d9c92f2ac658975050a6b162725d448ac4cf1e
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-02-25 18:15:44 -0300

    Alterando lista

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -30,37 +30,62 @@ export function HomePage() {
       });
   }, []);
 
-  const navigate = useNavigate();
-
   return (
     <ContainerApp titulo="Cardapio">
       <Row className="mt-3">
         {(data.length === 0) ? (
-          <Col md={12}>
-            <Titulo tag="h1">Lista Vazia</Titulo>
-          </Col>
+          <ListaVazia />
         ) : (
-          <Col md={12}>
-            {data.map((item, index) => {
-              return (
-                <ListGroup
-                  className="mb-3 pointer-event"
-                  key={index}
-                  onClick={() => navigate(`/refeicao/${item.id}`)}
-                >
-                  <ListGroupItem className="bg-info text-black">
-                    <Titulo tag="h2">{item.nome}</Titulo>
-                  </ListGroupItem>
-                  <ListGroupItem className="d-flex flex-row justify-content-between bg-info text-black">
-                    <Titulo tag="h3" className="fw-bold">Preço (R$)</Titulo>
-                    <Titulo tag="h3">{FormataValorMonetarioTexto(item.preco)}</Titulo>
-                  </ListGroupItem>
-                </ListGroup>
-              );
-            })}
-          </Col>
+          <Lista data={data} />
         )}
       </Row>
     </ContainerApp>
+  );
+}
+
+interface ListaProps {
+  data: DataTypes[];
+}
+
+function Lista(props: ListaProps) {
+  return (
+    <Col md={12}>
+      {props.data.map((item, index) => {
+        return (
+          <ListaItem key={index} data={item} />
+        );
+      })}
+    </Col>
+  );
+}
+
+interface ListaItemProps  {
+  data: DataTypes;
+}
+
+function ListaItem(props: ListaItemProps) {
+  const navigate = useNavigate();
+
+  return (
+    <ListGroup
+      className="mb-3"
+      onClick={() => navigate(`/refeicao/${props.data.id}`)}
+    >
+      <ListGroupItem className="bg-info text-black">
+        <Titulo tag="h2">{props.data.nome}</Titulo>
+      </ListGroupItem>
+      <ListGroupItem className="d-flex flex-row justify-content-between bg-info text-black">
+        <Titulo tag="h3" className="fw-bold">Preço (R$)</Titulo>
+        <Titulo tag="h3">{FormataValorMonetarioTexto(props.data.preco)}</Titulo>
+      </ListGroupItem>
+    </ListGroup>
+  );
+}
+
+function ListaVazia() {
+  return (
+    <Col md={12}>
+      <Titulo tag="h1">Lista Vazia</Titulo>
+    </Col>
   );
 }
