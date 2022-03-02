6c5b5781568723e3ee9dcd548bbfba211747bfed
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-03-01 22:12:28 -0300

    adicionando funções ConverteStringParaArrayObjetos e  ConverteStringParaArrayObjetos2

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,3 +1,21 @@
 export function FormataValorMonetarioTexto(valor: number) {
   return valor.toFixed(2).toString().replace('.', ',');
 }
+
+/**
+ * Converte string lista de ingredientes para uma array de objetos
+ */
+ export function ConverteStringParaArrayObjetos(texto: string) {
+  let converteParaArraySimples = texto.split(';');
+  let converteParaArrayDeObjetos = converteParaArraySimples.map(item => {
+    return { nome: item };
+  });
+  return converteParaArrayDeObjetos;
+}
+
+/**
+ * Converte string lista de ingredientes para uma array de objetos
+ */
+export function ConverteStringParaArrayObjetos2(texto: string) {
+  return JSON.parse(texto);
+}
