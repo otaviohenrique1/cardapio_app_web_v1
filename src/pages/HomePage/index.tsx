46134d1864064f05fd09fb907b980d79d19ca977
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-03-03 22:01:45 -0300

    refatorando parte da lista

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,19 +1,11 @@
 import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { Col, ListGroup, ListGroupItem, Row } from "reactstrap";
+import { Row } from "reactstrap";
 import { ContainerApp } from "../../components/ContainerApp";
-import { Titulo } from "../../components/Titulo";
+import { Lista, ListaVazia } from "../../components/Lista";
 import api from "../../utils/api";
-import { FormataValorMonetarioTexto } from "../../utils/utils";
-
-interface DataTypes {
-  id: string;
-  nome: string;
-  preco: number;
-}
 
 export function HomePage() {
-  const [data, setData] = useState<DataTypes[]>([]);
+  const [data, setData] = useState<ListaRefeicaoTypes[]>([]);
 
   useEffect(() => {
     api.get('/refeicao')
@@ -40,52 +32,5 @@ export function HomePage() {
         )}
       </Row>
     </ContainerApp>
-  );
-}
-
-interface ListaProps {
-  data: DataTypes[];
-}
-
-function Lista(props: ListaProps) {
-  return (
-    <Col md={12}>
-      {props.data.map((item, index) => {
-        return (
-          <ListaItem key={index} data={item} />
-        );
-      })}
-    </Col>
-  );
-}
-
-interface ListaItemProps  {
-  data: DataTypes;
-}
-
-function ListaItem(props: ListaItemProps) {
-  const navigate = useNavigate();
-
-  return (
-    <ListGroup
-      className="mb-3"
-      onClick={() => navigate(`/refeicao/${props.data.id}`)}
-    >
-      <ListGroupItem className="bg-info text-black">
-        <Titulo tag="h2">{props.data.nome}</Titulo>
-      </ListGroupItem>
-      <ListGroupItem className="d-flex flex-row justify-content-between bg-info text-black">
-        <Titulo tag="h3" className="fw-bold">Preço (R$)</Titulo>
-        <Titulo tag="h3">{FormataValorMonetarioTexto(props.data.preco)}</Titulo>
-      </ListGroupItem>
-    </ListGroup>
-  );
-}
-
-function ListaVazia() {
-  return (
-    <Col md={12}>
-      <Titulo tag="h1">Lista Vazia</Titulo>
-    </Col>
   );
 }
