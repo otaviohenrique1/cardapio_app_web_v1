4bb906f6fc961cdf6ff67e0a908e9a26fe8410d4
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-14 23:17:43 -0300

    organizando imports adicionando rotas:   Login   ClienteCadastro   ClienteDados   ClienteEdicao

--- src/pages/routes.tsx
+++ src/pages/routes.tsx
@@ -1,14 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { HomePage } from "./HomePage";
+import { Login } from "./Login";
 import { Pagina404 } from "./Pagina404";
-import { RefeicaoPage } from "./RefeicaoPage";
+import { RefeicaoDados } from "./RefeicaoDados";
+import { ClienteCadastro } from "./Cliente/ClienteCadastro";
+import { ClienteDados } from "./Cliente/ClienteDados";
+import { ClienteEdicao } from "./Cliente/ClienteEdicao";
 
 export function RoutesApp() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/refeicao/:id" element={<RefeicaoPage />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/cliente/cadastro" element={<ClienteCadastro/>} />
+        <Route path="/cliente/:id" element={<ClienteDados/>} />
+        <Route path="/cliente/:id/edicao" element={<ClienteEdicao />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/refeicao/:id" element={<RefeicaoDados />} />
         <Route element={<Pagina404 />} path="*" />
       </Routes>
     </BrowserRouter>
