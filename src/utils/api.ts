df311a521030ef1b10d9f6e12979e879e98428fe
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-14 23:16:20 -0300

    adicionadno constante

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,5 +1,11 @@
 import axios from "axios";
 
+/**
+ * Arrumar logica do ID da empresa cliente.
+ * ID pego no banco de dados
+ */
+export const id_empresa_cliente = '1';
+
 const api = axios.create({
   baseURL: 'http://localhost:3333/'
 });
