783c8143f786b2f87207bac20ab5a74e4cf5ff61
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-14 23:15:02 -0300

    reformulando arquivo

--- src/types/types.d.ts
+++ src/types/types.d.ts
@@ -1,13 +1,103 @@
-interface ListaRefeicaoTypes {
+/* Types */
+type ButtonColors =
+  "primary" |
+  "secondary" |
+  "success" |
+  "danger" |
+  "warning" |
+  "info" |
+  "light" |
+  "dark" |
+  "link";
+
+type DataHoraFormatos =
+  "HH:mm" |
+  "HH:mm:ss" |
+  "dd/MM/yyyy" |
+  "yyyy-MM-dd" |
+  'yyyy-MM-dd HH:mm' |
+  'dd/MM/yyyy HH:mm' |
+  'yyyy-MM-dd HH:mm:ss' |
+  'dd/MM/yyyy HH:mm:ss';
+
+type CampoInputTypes =
+  "text" |
+  "number" |
+  "email" |
+  "password";
+
+/* Parte do Login */
+interface LoginTypes {
+  email: string;
+  senha: string;
+}
+
+interface UsuarioLogadoTypes {
   id: string;
+  nome: string;
+}
+
+/* Parte do Usuario */
+interface UsuarioTypes {
+  nome: string;
+  email: string;
+  senha: string;
+}
+
+interface UsuarioDadosTypes extends UsuarioTypes {
+  id: string;
+  codigo: string;
+  data_cadastro: string;
+  data_modificacao_cadastro: string;
+}
+
+/* Parte da Refeicao */
+interface RefeicaoBaseTypes {
   nome: string;
   preco: number;
 }
 
+interface RefeicaoTypes extends RefeicaoBaseTypes {
+  nome: string;
+  preco: string | number;
+  descricao: string;
+  ingredientes: Ingredientes[];
+  imagens: File[];
+}
+
+// interface RefeicaoDadosTypes extends RefeicaoTypes {
+//   id: string;
+//   codigo: string;
+//   data_cadastro: string;
+//   data_modificacao_cadastro: string;
+// }
+
+interface RefeicaoListaTypes extends RefeicaoBaseTypes {
+  id: string;
+}
+
+interface Ingredientes {
+  nome: string;
+}
+
+/*
+interface Ingredientes {
+  nome: string;
+}
+
 interface RefeicaoTypes {
   nome: string;
-  preco: number;
-  ingredientes: { 
-    nome: string
-  }[];
+  preco: string | number;
+  ingredientes: Ingredientes[];
+  descricao: string;
+  ativo: string | boolean;
+  imagens: File[];
 }
+
+interface RefeicaoDadosTypes extends RefeicaoTypes {
+  id: string;
+  codigo: string;
+  data_cadastro: string;
+  data_modificacao_cadastro: string;
+}
+*/
--- src/utils/constantes.ts
+++ src/utils/constantes.ts
@@ -1,5 +1,50 @@
+export const EMAIL_INVALIDO = "Email invalido";
+export const valor_minimo_carateres = 8;
+export const MINIMO_CARACTERES = `Minimo ${valor_minimo_carateres} carateres`;
+export const valor_maximo_carateres = 64;
+export const MAXIMO_CARACTERES = `Maximo ${valor_maximo_carateres} carateres`;
+export const valor_minimo_ingredientes = 1;
+export const MINIMO_INGREDIENTES = `Minimo ${valor_minimo_ingredientes} carateres`;
+
+export const FORMATO_HORA_1 = 'HH:mm';
+export const FORMATO_HORA_2 = 'HH:mm:ss';
+export const FORMATO_DATA_1 = 'dd/MM/yyyy';
+export const FORMATO_DATA_2 = 'yyyy-MM-dd';
+export const FORMATO_DATA_COM_HORA_1 = 'yyyy-MM-dd HH:mm';
+export const FORMATO_DATA_COM_HORA_2 = 'dd/MM/yyyy HH:mm';
+export const FORMATO_DATA_COM_HORA_3 = 'yyyy-MM-dd HH:mm:ss';
+export const FORMATO_DATA_COM_HORA_4 = 'dd/MM/yyyy HH:mm:ss';
+
 export const valoresIniciaisRefeicao: RefeicaoTypes = {
   nome: "",
   preco: 0,
-  ingredientes: []
+  ingredientes: [],
+  descricao: "",
+  imagens: []
+};
+
+export const valoresIniciaisFormularioUsuario: UsuarioTypes = {
+  nome: "",
+  email: "",
+  senha: "",
+};
+
+export const dadosIniciaisFormularioLogin: LoginTypes = {
+  email: "",
+  senha: ""
+};
+
+export const valoresIniciaisUsuarioDados: UsuarioDadosTypes = {
+  id: "",
+  nome: "",
+  email: "",
+  senha: "",
+  data_cadastro: "",
+  codigo: "",
+  data_modificacao_cadastro: ""
+};
+
+export const dadosIniciaisUsuarioLogado: UsuarioLogadoTypes = {
+  id: '',
+  nome: ''
 };
