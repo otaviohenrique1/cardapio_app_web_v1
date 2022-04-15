68d20d64be36aa0f9e96dbd3c4e7265f3ce56ecc
Otavio Henrique <42388160+otaviohenrique1@users.noreply.github.com>
2022-04-15 19:59:36 -0300

    adicionando AlertMensagemErro

--- src/components/Campos/CampoInput/index.tsx
+++ src/components/Campos/CampoInput/index.tsx
@@ -1,6 +1,7 @@
 import { Field } from "formik";
-import { Col, Label, Alert } from "reactstrap";
+import { Col, Label } from "reactstrap";
 import { ColumnProps } from "reactstrap/types/lib/Col";
+import { AlertMensagemErro } from "../../AlertMensagem/AlertMensagemErro";
 
 interface CampoInputProps {
   md: ColumnProps;
@@ -17,10 +18,21 @@ interface CampoInputProps {
 export function CampoInput(props: CampoInputProps) {
   return (
     <Col md={props.md} className="d-flex flex-column mt-3">
-      <Label className="form-label" htmlFor={props.id}>{props.label}</Label>
-      <Field className="form-control" id={props.id} name={props.name}
-        type={props.type} value={props.value} placeholder={props.placeholder} />
-      {props.error && props.touched ? (<Alert color="danger">{props.error}</Alert>) : null}
+      <Label
+        className="form-label"
+        htmlFor={props.id}
+      >{props.label}</Label>
+      <input type="text" />
+      <Field
+        className="form-control"
+        id={props.id}
+        name={props.name}
+        type={props.type}
+        value={props.value}
+        placeholder={props.placeholder}
+      />
+      {props.error && props.touched
+        ? (<AlertMensagemErro>{props.error}</AlertMensagemErro>) : null}
     </Col>
   );
 }
